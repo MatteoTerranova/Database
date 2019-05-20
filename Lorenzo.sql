0ccb4e21195e814a1ad64b9a7b0dce9535763dd0
Nicola Gastaldello <Nicola@Nicolas-MBP.homenet.telecomitalia.it>
2019-05-20 17:20:16 +0200

    Table Validate and Approve

---
+++ Lorenzo.sql
@@ -0,0 +1,17 @@
+create TABLE Approve (
+  DocumentID UUID NOT NULL,
+	FiscalCode VARCHAR(16) NOT NULL,
+	TimeStamp TIMESTAMP NOT NULL,
+	PRIMARY KEY (DocumentID)
+  FOREIGN KEY (FiscalCode) REFERENCES Employee(FiscalCode),
+  FOREIGN KEY (DocumentID) REFERENCES Document(DocumentID)
+);
+
+create TABLE Validate (
+  DocumentID UUID NOT NULL,
+	FiscalCode VARCHAR(16) NOT NULL,
+	TimeStamp TIMESTAMPTZ NOT NULL,
+	PRIMARY KEY (DocumentID)
+  FOREIGN KEY (FiscalCode) REFERENCES Employee(FiscalCode),
+  FOREIGN KEY (DocumentID) REFERENCES Document(DocumentID)
+);
