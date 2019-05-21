648744a69aad8e8dcaeb7481b37b8dcff7d459c9
MatteoTerranova <terranova.matteo@gmail.com>
2019-05-21 15:36:13 +0200

    Add .sql file in which the tables are created in the correct order

---
+++ MergedDBCreationFiles.sql
@@ -0,0 +1,153 @@
+-- ZIP has VARCHAR(5) because postal code length is always 5 (in Italy)
+-- Contact
+CREATE TABLE Contact(
+	FiscalCode VARCHAR(16),
+	Name VARCHAR NOT NULL,
+	Surname VARCHAR NOT NULL,
+	BirthDate DATE,
+	Email VARCHAR,
+	PhoneNumber VARCHAR NOT NULL,
+	ZIP VARCHAR(5) NOT NULL,
+	Street VARCHAR,
+	Number SMALLINT,
+	CONSTRAINT Address CHECK ((Street is NULL AND Number is NULL) OR (Street is NOT  NULL AND Number is NOT NULL)),
+	PRIMARY KEY (FiscalCode)
+);
+
+-- Employee
+CREATE TABLE Employee(
+	FiscalCode VARCHAR(16),
+	Username VARCHAR NOT NULL,
+	Password VARCHAR NOT NULL,
+	IBAN VARCHAR NOT NULL,
+	HourlyWage float(2) NOT NULL,
+	Role VARCHAR NOT NULL,
+
+	PRIMARY KEY (FiscalCode),
+	FOREIGN KEY (FiscalCode) REFERENCES Contact(FiscalCode)
+);
+
+--Customer
+CREATE TABLE Customer(
+	FiscalCode VARCHAR(16),
+	Note VARCHAR NOT NULL,
+
+	PRIMARY KEY (FiscalCode),
+	FOREIGN KEY (FiscalCode) REFERENCES Contact(FiscalCode)
+);
+
+--Project
+
+--Request
+CREATE TABLE Request(
+	FiscalCode VARCHAR(16),
+	ProjectID UUID,
+
+	PRIMARY KEY (FiscalCode, ProjectID),
+	FOREIGN KEY (FiscalCode) REFERENCES Customer(FiscalCode),
+	FOREIGN KEY (FiscalCode) REFERENCES ProjectID(FiscalCode)
+);
+
+--Department
+
+--Template
+CREATE TABLE Template	(
+	TemplateID UUID,
+	Description VARCHAR,
+	IsRoot BOOLEAN NOT NULL,	
+	PRIMARY KEY (TemplateID)
+);
+
+--Order
+CREATE TABLE Order	(
+	Child UUID NOT NULL,
+	Parent UUID NOT NULL
+	PRIMARY KEY (Child, Parent)
+	FOREIGN KEY (Child) REFERENCES Template(TemplateID),
+	FOREIGN KEY (Parent) REFERENCES Template(TemplateID)
+);
+
+--Task
+CREATE TABLE Task	(
+	TaskID UUID,
+	IsRoot BOOLEAN NOT NULL,
+	Description VARCHAR,
+	StartDate DATE NOT NULL,
+	EndDate DATE,
+	TemplateID UUID NOT NULL,
+	Name VARCHAR(50) NOT NULL,	
+	PRIMARY KEY (TaskID),
+	FOREIGN KEY (TemplateID) REFERENCES Template(TemplateID),
+	FOREIGN KEY (Name) REFERENCES Department(Name)
+);
+
+--Compose
+
+--TimeSlot
+CREATE TABLE TimeSlot(
+TimeSlotID UUID,
+TaskID UUID,
+TimeStamp TIMESTAMPTZ NOT NULL,
+FiscalCode TEXT NOT NULL,
+Notes TEXT,
+Hours FLOAT NOT NULL,
+HourlyWage FLOAT NOT NULL,
+PRIMARY KEY(TimeSlot ID),
+FOREIGN KEY(TaskID) REFERENCES Task(TaskID),
+FOREIGN KEY(FiscalCode) REFERENCES Employee(FiscalCode)
+);
+
+--Expense
+CREATE TABLE Expense(
+Type TEXT,
+PRIMARY KEY (Type)
+);
+
+--Has
+CREATE TABLE Has(
+TimeSlot ID UUID NOT NULL,
+Type TEXT NOT NULL,
+Cost FLOAT NOT NULL,
+Description TEXT,
+HourlyWage FLOAT NOT NULL,
+PRIMARY KEY (TimeSlotID, Type),
+FOREIGN KEY (TimeSlotID) REFERENCES Timeslot(TimeSlotID),
+FOREIGN KEY (Type) REFERENCES Expense (Type)
+);
+
+--Document
+CREATE TABLE Document(
+	DocumentID UUID,
+	Title TEXT NOT NULL,
+	Content BLOB NOT NULL,
+	TimeStamp TIMESTAMPTZ NOT NULL,
+	PRIMARY KEY (DocumentID)
+);
+
+--Version
+CREATE TABLE Version(
+	DocumentID_Predecessor UUID,
+	DocumentID_Successor UUID,
+	PRIMARY KEY (DocumentID_Predecessor),
+	FOREIGN KEY (DocumentID_Predecessor) REFERENCES Document(DocumentID)
+);
+
+--Validate 
+create TABLE Validate (
+  DocumentID UUID NOT NULL,
+	FiscalCode VARCHAR(16) NOT NULL,
+	TimeStamp TIMESTAMPTZ NOT NULL,
+	PRIMARY KEY (DocumentID)
+  FOREIGN KEY (FiscalCode) REFERENCES Employee(FiscalCode),
+  FOREIGN KEY (DocumentID) REFERENCES Document(DocumentID)
+);
+
+--Approve
+create TABLE Approve (
+  DocumentID UUID NOT NULL,
+	FiscalCode VARCHAR(16) NOT NULL,
+	TimeStamp TIMESTAMP NOT NULL,
+	PRIMARY KEY (DocumentID)
+  FOREIGN KEY (FiscalCode) REFERENCES Employee(FiscalCode),
+  FOREIGN KEY (DocumentID) REFERENCES Document(DocumentID)
+);
