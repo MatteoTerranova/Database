99449240b1732c5b7106ffa6dfaff2f274115232
GiovanniGallinaro <giovanni_gallinaro@libero.it>
2019-05-21 15:41:03 +0200

    SPIDERMAN!

--- Lorenzo.sql
+++ Lorenzo.sql
@@ -1,7 +1,7 @@
 create TABLE Approve (
   DocumentID UUID NOT NULL,
 	FiscalCode VARCHAR(16) NOT NULL,
-	TimeStamp TIMESTAMP NOT NULL,
+	TimeStamp TIMESTAMPZ NOT NULL,
 	PRIMARY KEY (DocumentID)
   FOREIGN KEY (FiscalCode) REFERENCES Employee(FiscalCode),
   FOREIGN KEY (DocumentID) REFERENCES Document(DocumentID)
