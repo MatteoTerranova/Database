b91d082f84cbc362d737fceb3675739259250bb0
GiovanniGallinaro <giovanni_gallinaro@libero.it>
2019-05-27 15:14:03 +0200

    SPIDERMAN!

--- CompletedFiles/db_population.sql
+++ CompletedFiles/db_population.sql
@@ -146,7 +146,7 @@ INSERT INTO Compose(Parent, Child, ProjectID) VALUES
 ('84a0ee99-4702-41de-8aff-185d257c9dc1','165b25b4-8b44-46eb-b8ca-4becc7e6ca7c','866adc89-5a04-463b-82a3-3bf0ad77491b'),
 ('84a0ee99-4702-41de-8aff-185d257c9dc1','35e0ef16-edee-4a30-9f46-80b0b959610c','866adc89-5a04-463b-82a3-3bf0ad77491b'),
 ('84a0ee99-4702-41de-8aff-185d257c9dc1','644932c2-c6c5-4872-b6a8-114634c6a472','866adc89-5a04-463b-82a3-3bf0ad77491b'),
-('84a0ee99-4702-41de-8aff-185d257c9dc1','850216a6-3c23-4aca-ad7d-ee0b1916bc7b','866adc89-5a04-463b-82a3-3bf0ad77491b'),
+('84a0ee99-4702-41de-8aff-185d257c9dc1','850216a6-3c23-4aca-ad7d-ee0b1916bc7b','866adc89-5a04-463b-82a3-3bf0ad77491b');
 
 --TimeSlot
 INSERT INTO TimeSlot(TimeSlotID, TaskID, CurTime, Notes, HourlyWage, FiscalCode, Hours) VALUES
@@ -168,7 +168,7 @@ INSERT INTO TimeSlot(TimeSlotID, TaskID, CurTime, Notes, HourlyWage, FiscalCode,
 ('31f1de04-e117-4463-8eef-ba0598502e66', '165b25b4-8b44-46eb-b8ca-4becc7e6ca7c','2018-05-10 10:32:25+01',NULL,'12.50','BRNGNN64G46H005D','3.5'),
 ('1a14cdb1-6d78-4266-b9ea-7e76d8822701', '35e0ef16-edee-4a30-9f46-80b0b959610c','2018-05-14 12:32:25+01',NULL,'12.50','BRNGNN64G46H005D','10.5'),
 ('2cae32d3-7bf1-435f-b851-9220fb8c2ca1', '644932c2-c6c5-4872-b6a8-114634c6a472','2018-05-17 18:32:25+01',NULL,'12.50','BRNGNN64G46H005D','9.0'),
-('7134af62-a1e2-43df-a150-1ec98e44c8fb', '850216a6-3c23-4aca-ad7d-ee0b1916bc7b','2018-05-21 15:32:25+01',NULL,'12.50','BRNGNN64G46H005D','7.5'),
+('7134af62-a1e2-43df-a150-1ec98e44c8fb', '850216a6-3c23-4aca-ad7d-ee0b1916bc7b','2018-05-21 15:32:25+01',NULL,'12.50','BRNGNN64G46H005D','7.5');
 
 --Expense
 INSERT INTO Expense(Type) VALUES
@@ -178,12 +178,12 @@ INSERT INTO Expense(Type) VALUES
 
 --Has
 INSERT INTO Has(TimeSlotID, Type, Cost, Description) VALUES
-('3e636e45-034a-4269-b868-a0a8958086d4','DesignMaterials','250.80','Paper and some other materials needed for the design'),
-('69aed574-6572-42f0-863e-c7ba2260d752','TripExpenses','540.12',NULL),
-('fe2ef85c-752b-4af5-ae67-dfc1f247f979','FuelExpenses','320.00','Fuel for 150 km');
+('69aed574-6572-42f0-863e-c7ba2260d752','DesignMaterials','250.80','Paper and some other materials needed for the design'),
+('12476d9b-058c-4a71-b33b-bdca507e53c6','TripExpenses','540.12',NULL),
+('31f1de04-e117-4463-8eef-ba0598502e66','FuelExpenses','320.00','Fuel for 150 km');
 
 --Document
-INSERT INTO Document(DocumentID, Title, Content, CurTime, TaskID, Producer) VALUES
+/*INSERT INTO Document(DocumentID, Title, Content, CurTime, TaskID, Producer) VALUES
 ('ef88126a-5133-4ce4-aea8-45a5781e54ca', 'After Inspection',?????,'2019-06-25 09:29:25+01','c769d3a6-41d1-4883-9edf-e74a977446ad','LRNBTT96C01D149A'),
 ('c7ba2260d752-4af5-034a-a0a8958086d4','Planimetry Computations',????????,'2019-04-25 10:08:25+01','7ba08242-0397-4137-9275-e18d8be8ef8e','RSSGVN76A15E189E'),
 ('ccdfba04-6725-49cb-8814-c214c35fe3ea', '2D final design',?????,'2019-05-26 13:15:25+01','6ef9878a-416e-4782-8e7e-7aa9dd42c140','RSSGVN76A15E189E'),
@@ -201,4 +201,4 @@ INSERT INTO ValidateDocument(DocumentID, FiscalCode, CurTime) VALUES
 --Shown two cases where one document is validated by the chief and another by the Area Manager
 INSERT INTO ApproveDocument(DocumentID, FiscalCode, CurTime) VALUES
 ('04cfbbab-14ea-4af5-ae67-dfc1f247f979','MNGSLV89H60Z100C','2019-05-27 12:25:25+01'),
-('c7ba2260d752-4af5-034a-a0a8958086d4','BRNGNN64G46H005D','2019-04-25 30:08:25+01');
+('c7ba2260d752-4af5-034a-a0a8958086d4','BRNGNN64G46H005D','2019-04-25 30:08:25+01');*/
