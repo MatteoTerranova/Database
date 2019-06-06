e7f91aa3b53e8a6e27773d21cf795c9cc9c2ef8d
GiovanniGallinaro <giovanni_gallinaro@libero.it>
2019-06-06 10:24:14 +0200

    Added possiblity to insert shifts

--- database-demo/src/main/webapp/js/ajax_project.js
+++ database-demo/src/main/webapp/js/ajax_project.js
@@ -109,6 +109,35 @@ function displayProjects2(data) {
 
 }	
 
+// *************************** SELECT PROJECT 3 *****************************
+
+// Ajax request
+$.ajax({
+  contentType: "application/json; charset=utf-8",
+  url: "rest/project",
+  success: displayProjects3
+});
+
+function displayProjects3(data) {
+	
+	// Log error message
+	if (data.message != null){
+		console.log(data.message);
+		console.log(data.message.error-code);
+		console.log(data.message.error-details);
+	}
+		
+	var select = document.getElementById("project3");
+	var list = data["resource-list"];
+	for (i = 0; i < list.length; i++) {
+		var option = document.createElement("option");
+		option.text = list[i].project.title;
+		select.add(option);
+	}
+
+}	
+
+
 // *************************** SELECT EMPLOYEE *****************************
 
 // Ajax request
@@ -156,6 +185,34 @@ function displayEmployees2(data) {
 	}
 		
 	var select = document.getElementById("employee2");
+	var list = data["resource-list"];
+	for (i = 0; i < list.length; i++) {
+		var option = document.createElement("option");
+		option.text = list[i].employee.name + " " + list[i].employee.surname;
+		select.add(option);
+	}
+
+}	
+
+// *************************** SELECT EMPLOYEE 3 *****************************
+
+// Ajax request
+$.ajax({
+  contentType: "application/json; charset=utf-8",
+  url: "rest/employee",
+  success: displayEmployees3
+});
+
+function displayEmployees3(data) {
+	
+	// Log error message
+	if (data.message != null){
+		console.log(data.message);
+		console.log(data.message.error-code);
+		console.log(data.message.error-details);
+	}
+		
+	var select = document.getElementById("employee3");
 	var list = data["resource-list"];
 	for (i = 0; i < list.length; i++) {
 		var option = document.createElement("option");
