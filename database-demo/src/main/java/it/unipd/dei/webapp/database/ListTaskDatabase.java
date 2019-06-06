bd97c39233660215b2d91cbda81f915c8704accb
Alessandro Cattapan <alecattapan22@gmail.com>
2019-06-06 15:57:30 +0200

    cacca

--- database-demo/src/main/java/it/unipd/dei/webapp/database/ListTaskDatabase.java
+++ database-demo/src/main/java/it/unipd/dei/webapp/database/ListTaskDatabase.java
@@ -18,7 +18,7 @@ public final class ListTaskDatabase {
 	/**
 	 * The SQL statement to be executed
 	 */
-	private static final String STATEMENT = "WITH RECURSIVE proj_subpart AS (SELECT Parent, Child, ProjectID, 1 AS Depth FROM Task as t INNER JOIN Compose AS C  ON t.taskID = C.Child WHERE isRoot = TRUE UNION ALL SELECT C.Parent, C.Child, C.ProjectID, Depth + 1 FROM proj_subpart AS pr INNER JOIN Compose AS C ON C.Parent = pr.Child) SELECT p.ProjectID AS Project_Name, t.taskID, tmt.TemplateID AS Task_Description, Depth, tmt.Description AS General_task_description FROM Project AS p INNER JOIN proj_subpart AS ps ON p.ProjectID = ps.ProjectID INNER JOIN Task AS t ON ps.Child = t.taskID INNER JOIN Template AS tmt ON t.TemplateID = tmt.TemplateID WHERE p.ProjectID = ? AND depth < 3 ORDER BY p.ProjectID, Depth ASC";
+	private static final String STATEMENT = "WITH RECURSIVE proj_subpart AS (SELECT Parent, Child, ProjectID, 1 AS Depth FROM Task as t INNER JOIN Compose AS C  ON t.taskID = C.Child WHERE isRoot = TRUE UNION ALL SELECT C.Parent, C.Child, C.ProjectID, Depth + 1 FROM proj_subpart AS pr INNER JOIN Compose AS C ON C.Parent = pr.Child) SELECT p.ProjectID AS Project_Name, t.taskID AS tasktask, tmt.TemplateID AS Task_Description, Depth, tmt.Description AS General_task_description FROM Project AS p INNER JOIN proj_subpart AS ps ON p.ProjectID = ps.ProjectID INNER JOIN Task AS t ON ps.Child = t.taskID INNER JOIN Template AS tmt ON t.TemplateID = tmt.TemplateID WHERE p.ProjectID = ? AND depth < 3 ORDER BY p.ProjectID, Depth ASC";
 
 	/**
 	 * The connection to the database
@@ -61,7 +61,7 @@ public final class ListTaskDatabase {
 			while (rs.next()) {
 				tasks.add(new Task( 
 						rs.getString("task_description"), 
-						UUID.fromString(rs.getString("t.taskID")),
+						UUID.fromString(rs.getString("tasktask")),
 						rs.getString("general_task_description"),
 						rs.getInt("Depth")));
 			}
